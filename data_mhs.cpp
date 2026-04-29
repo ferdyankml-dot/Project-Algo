f5708b0e751733736db0973e4a511c59e79233b5
saefulbahar <berkahmoga247>
2026-04-29 20:17:02 +0700

    first commit

--- data_mhs.cpp
+++ data_mhs.cpp
@@ -1,122 +1,193 @@
-	#include <iostream>
-	#include <iomanip>
-	using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <limits>
+using namespace std;
 
-			struct Mahasiswa {
-				int no;
-				string nim, nama, domisili, email, nohp;
-				char jenis_kelamin;
-				float ipk;
+struct Mahasiswa {
+    int no;
+    string nim, nama, domisili, email, nohp;
+    char jenis_kelamin;
+    float ipk;
 
-				Mahasiswa *prev;
-				Mahasiswa *next;
-			};
+    Mahasiswa *prev;
+    Mahasiswa *next;
+};
 
-			Mahasiswa *head = NULL, *tail = NULL;
-			int autoNo = 1;
+Mahasiswa *head = NULL, *tail = NULL;
+int autoNo = 1;
 
-			// menu 
-			void tampilMenu() {
-				cout << endl;
-				cout << "==========================================" << endl;
-				cout << "     SISTEM DATA MAHASISWA SEDERHANA      " << endl;
-				cout << "==========================================" << endl;
-				cout << "1. tambah data mahasiswa" << endl;
-				cout << "2. lihat data mahasiswa" << endl;
-				cout << "0. logout" << endl;
-				cout << "------------------------------------------" << endl;
-				cout << "pilih menu: ";
-			}
+// menu utaama
+void tampilMenu() {
+    cout << endl;
+    cout << "==========================================" << endl;
+    cout << "     SISTEM DATA MAHASISWA SEDERHANA      " << endl;
+    cout << "==========================================" << endl;
+    cout << "1. tambah data mahasiswa" << endl;
+    cout << "2. lihat data mahasiswa" << endl;
+    cout << "3. hapus data mahasiswa" << endl;
+    cout << "4. urutkan data mahasiswa" << endl;
+    cout << "5. cari data mahasiswa" << endl;
+    cout << "6. logout" << endl;
+    cout << "------------------------------------------" << endl;
+    cout << "pilih menu: ";
+}
 
-			// tambah data 
-			void tambahData() {
-				Mahasiswa *baru = new Mahasiswa();
+// untuk simpan file
+void simpanData() {
+    ofstream file("data.txt");
 
-				baru->no = autoNo++;
+    Mahasiswa *temp = head;
+    while (temp != NULL) {
+        file << temp->no << "|"
+             << temp->nim << "|"
+             << temp->nama << "|"
+             << temp->jenis_kelamin << "|"
+             << temp->domisili << "|"
+             << temp->email << "|"
+             << temp->nohp << "|"
+             << temp->ipk << endl;
 
-				cout << "nim: "; cin >> baru->nim;
-				cout << "nama: "; cin.ignore(); getline(cin, baru->nama);
-				cout << "jenis kelamin (L/P): "; cin >> baru->jenis_kelamin;
-				cout << "domisili: "; cin.ignore(); getline(cin, baru->domisili);
-				cout << "email: "; cin >> baru->email;
-				cout << "no hp: "; cin >> baru->nohp;
-				cout << "ipk: "; cin >> baru->ipk;
+        temp = temp->next;
+    }
 
-				baru->next = NULL;
-				baru->prev = NULL;
+    file.close();
+}
 
-				if (head == NULL) {
-					head = tail = baru;
-				} else {
-					tail->next = baru;
-					baru->prev = tail;
-					tail = baru;
-				}
+// proses memasukan data
+void loadData() {
+    ifstream file("data.txt");
 
-				cout << endl;
-				cout << "data berhasil ditambahkan!" << endl;
-			}
+    while (!file.eof()) {
+        Mahasiswa *baru = new Mahasiswa();
 
-			// lihat data
-			void lihatData() {
-				Mahasiswa *temp = head;
+        file >> baru->no;
+        file.ignore();
 
-				if (temp == NULL) {
-					cout << endl;
-					cout << "data masih kosong" << endl;
-					return;
-				}
+        getline(file, baru->nim, '|');
+        getline(file, baru->nama, '|');
 
-				cout << endl;
-				cout << "==============================================================" << endl;
-				cout << left << setw(5) << "No"
-					 << setw(15) << "nim"
-					 << setw(20) << "nama"
-					 << setw(5) << "jenis kelamin"
-					 << setw(10) << "ipk" << endl;
-				cout << "==============================================================" << endl;
+        file >> baru->jenis_kelamin;
+        file.ignore();
 
-				while (temp != NULL) {
-					cout << left << setw(5) << temp->no
-						 << setw(15) << temp->nim
-						 << setw(20) << temp->nama
-						 << setw(5) << temp->jenis_kelamin
-						 << setw(10) << temp->ipk << endl;
+        getline(file, baru->domisili, '|');
+        getline(file, baru->email, '|');
+        getline(file, baru->nohp, '|');
 
-					temp = temp->next;
-				}
+        file >> baru->ipk;
+        file.ignore();
 
-				cout << "==============================================================" << endl;
-			}
+        if (file.fail()) break;
 
-			// main
-			int main() {
-				int pilih;
+        baru->next = NULL;
+        baru->prev = NULL;
 
-				do {
-					tampilMenu();
-					cin >> pilih;
+        if (head == NULL) {
+            head = tail = baru;
+        } else {
+            tail->next = baru;
+            baru->prev = tail;
+            tail = baru;
+        }
 
-					if (pilih == 1) {
-						cout << endl;
-						cout << "===== INPUT DATA MAHASISWA =====" << endl;
-						tambahData();
-					} 
-					else if (pilih == 2) {
-						cout << endl;
-						cout << "===== DATA MAHASISWA =====" << endl;
-						lihatData();
-					} 
-					else if (pilih == 0) {
-						cout << endl;
-						cout << "trima kasih bolo " << endl;
-					} 
-					else {
-						cout << endl;
-						cout << "mFenu tidak ada" << endl;
-					}
+        autoNo = baru->no + 1;
+    }
 
-				} while (pilih != 0);
+    file.close();
+}
 
-				return 0;
-			}
+// tambaha data
+void tambahData() {
+    Mahasiswa *baru = new Mahasiswa();
+
+    baru->no = autoNo++;
+
+    cout << "nim: "; cin >> baru->nim;
+	cout << "nama: "; cin.ignore(); getline(cin, baru->nama);
+	cout << "jenis kelamin (L/P): "; cin >> baru->jenis_kelamin;
+	cout << "domisili: "; cin.ignore(); getline(cin, baru->domisili);
+	cout << "email: "; cin >> baru->email;
+	cout << "no hp: "; cin >> baru->nohp;
+	cout << "ipk: "; cin >> baru->ipk;
+	
+    baru->next = NULL;
+    baru->prev = NULL;
+
+    if (head == NULL) {
+        head = tail = baru;
+    } else {
+        tail->next = baru;
+        baru->prev = tail;
+        tail = baru;
+    }
+
+    cout << endl;
+    cout << "data berhasil ditambahkan" << endl;
+}
+
+// lihat data
+void lihatData() {
+    Mahasiswa *temp = head;
+
+    if (temp == NULL) {
+        cout << endl;
+        cout << "data masih kosong" << endl;
+        return;
+    }
+
+    cout << endl;
+    cout << "==============================================================" << endl;
+    cout << left << setw(5) << "No"
+         << setw(15) << "nim"
+         << setw(20) << "nama"
+         << setw(5) << "JK"
+         << setw(10) << "ipk" << endl;
+    cout << "==============================================================" << endl;
+
+    while (temp != NULL) {
+        cout << left << setw(5) << temp->no
+             << setw(15) << temp->nim
+             << setw(20) << temp->nama
+             << setw(5) << temp->jenis_kelamin
+             << setw(10) << temp->ipk << endl;
+
+        temp = temp->next;
+    }
+
+    cout << "==============================================================" << endl;
+}
+
+// main
+int main() {
+    int pilih;
+
+    loadData(); // ambil data dari file
+
+    do {
+        tampilMenu();
+        cin >> pilih;
+
+        if (pilih == 1) {
+            cout << endl;
+            cout << "===== INPUT DATA MAHASISWA =====" << endl;
+            tambahData();
+        } 
+        else if (pilih == 2) {
+            cout << endl;
+            cout << "===== DATA MAHASISWA =====" << endl;
+            lihatData();
+        } 
+        else if (pilih == 0) {
+            simpanData(); // simpan ke file
+            cout << endl;
+            cout << "trima kasih bolo" << endl;
+        } ``
+        else {
+            cout << endl;
+            cout << "menu tidak ada" << endl;
+        }
+
+    } while (pilih != 0);
+
+    return 0;
+}
